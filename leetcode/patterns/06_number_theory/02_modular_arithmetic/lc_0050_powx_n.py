6e558c083714a7f476606c1f459ac4550806a9a9
taintpro98 <taideptrai02@gmail.com>
2026-02-23 09:16:48 +0700

    my pow

--- leetcode/patterns/06_number_theory/02_modular_arithmetic/lc_0050_powx_n.py
+++ leetcode/patterns/06_number_theory/02_modular_arithmetic/lc_0050_powx_n.py
@@ -4,4 +4,23 @@ https://leetcode.com/problems/powx-n/
 
 Pattern:
 Fast Exponentiation (Binary Exponentiation)
+
+Key Insight:
+- x^n = (x^2)^(n/2) if n is even
+- x^n = x * (x^2)^(n/2) if n is odd
+- Time: O(log n), Space: O(1)
 """
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        k = n // 2
+        r = n % 2
+        a = self.myPow(x, k)
+        return a * a * self.myPow(x, r)
